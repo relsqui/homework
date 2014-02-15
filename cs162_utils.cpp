7f1c3fc965b79295b9742f33debc4dbe4c2ebfa1
Finn Ellis <relsqui@chiliahedron.com>
2014-02-15 11:48:02 -0800

    Comments and formatting.

--- cs162_utils.cpp
+++ cs162_utils.cpp
@@ -1,6 +1,10 @@
+// Finn Ellis - CS 162
+// General utilities common to 162 projects.
+
 #include <iostream>
 #include <cctype>
 #include <cstring>
+
 using namespace std;
 
 
@@ -10,6 +14,9 @@ void empty_cin() {
 
 
 void read_chars(char array[], int length, const char prompt[]) {
+    // Prints a prompt, then reads at most length characters into a
+    // character array from cin. Complains and gives the user a chance
+    // to try again if they entered too much.
     bool toolong = false;
     do {
         cout << prompt;
@@ -33,6 +40,8 @@ void read_chars(char array[], int length, const char prompt[]) {
 
 
 int read_int(const char prompt[]) {
+    // Supplies a prompt, then reads an integer from cin and returns it.
+    // If nothing was entered, returns -1.
     int number = 0;
     cout << prompt;
     if (cin.peek() == '\n') {
@@ -47,6 +56,8 @@ int read_int(const char prompt[]) {
 
 
 void strupper(const char in_string[], char out_string[]) {
+    // Puts an uppercase version of the first character array into the second
+    // character array. Supply the same array twice to convert in place.
     int i = 0;
     for (; i < strlen(in_string); i++) {
         out_string[i] = toupper(in_string[i]);
@@ -56,6 +67,8 @@ void strupper(const char in_string[], char out_string[]) {
 
 
 void strlower(const char in_string[], char out_string[]) {
+    // Puts a lowercase version of the first character array into the second
+    // character array. Supply the same array twice to convert in place.
     int i = 0;
     for (; i < strlen(in_string); i++) {
         out_string[i] = tolower(in_string[i]);
@@ -65,6 +78,9 @@ void strlower(const char in_string[], char out_string[]) {
 
 
 bool partial_match(const char input[], const char target[]) {
+    // Compares input, case-insensitively, to target, up to the length of the
+    // input. Returns true if they match and false otherwise. This is for
+    // correctly matching abbreviations (e.g. "y" and "YE" both match "yes").
     char * input_lower;
     char * target_lower;
     if (!strlen(input)) {
@@ -87,6 +103,9 @@ bool partial_match(const char input[], const char target[]) {
 
 
 bool boolean_prompt(const char prompt[]) {
+    // Displays a prompt to the user and then reads a response; if it matches
+    // "yes" (by partial_match), returns true, and if it matches "no," returns
+    // false. Complains and gives the user another chance if neither matches.
     char response[4] = "\0";
     do {
         cout << prompt;
